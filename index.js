d92571dc7bad01f0a8d91262b5dae2b273dd4a97
chandan632 <chandan737@gmail.com>
2022-07-16 18:36:28 +0530

    Token API

--- index.js
+++ index.js
@@ -10,4 +10,15 @@ app.get('/', (req, res) => {
     })
 })
 
+/*
+ @param Token -> Token is whatever user typed after /api/v1/
+ @return Json data -> Whatever user typed server send it's back
+*/
+app.get('/api/v1/:token', (req, res) => {
+    res.status(200).json({
+        token: req.params.token,
+        msg: "Record retrieved successfully!"
+    })
+})
+
 app.listen(PORT, () => console.log(`Server is up and running on PORT ${PORT}`))
