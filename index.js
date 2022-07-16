6c58d5684f576bf6c996d77407cfb83ad259486a
chandan632 <chandan737@gmail.com>
2022-07-16 18:50:23 +0530

    Instagram API

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { application } = require("express")
 const express = require("express")
 
 const app = express()
@@ -19,6 +20,20 @@ app.get('/api/v1/:token', (req, res) => {
         token: req.params.token,
         msg: "Record retrieved successfully!"
     })
+})
+
+/*
+  @return Instagram data of User
+*/
+app.get('api/v1/instagram', (req, res) => {
+    const instagramData = {
+        api: "Instagram API",
+        username: "_chandan__rout",
+        followers: 90,
+        following: 88,
+        date: Date.now() 
+    }
+    res.status(200).json(instagramData)
 })
 
 app.listen(PORT, () => console.log(`Server is up and running on PORT ${PORT}`))
